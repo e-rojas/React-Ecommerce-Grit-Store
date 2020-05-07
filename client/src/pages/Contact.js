db7b86c9b6fcfcedcdc8a50bf568b82301f6e786
EJ <38365682+e-rojas@users.noreply.github.com>
2020-05-07 12:16:49 -0300

    Form page with componets added

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -2,8 +2,41 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <div>
-      <h1>CONTACT CACTU</h1>
+    <div style={{height:'600px'}}  className="container-fluid  d-flex flex-column justify-content-center align-items-center ">
+      <form style={{width:'400px'}} className='border border-dark p-5 rounded'>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+       
+        <div className="form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+         
+          <label className="form-check-label" htmlFor="exampleCheck1">
+           Sign Me Up!
+          </label>
+        </div>
+        <div className="form-group">
+            <label for='textArea' >Message</label>
+            <textarea id='textArea' className='form-control'  rows="3"></textarea>
+          </div>
+        <button type="submit" className="btn btn-primary mt-4">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
